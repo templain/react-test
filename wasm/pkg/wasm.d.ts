548daa3efcfb1321bd60dbfa932d0d0919b42f4e
grandmacks <grandmacks@gmail.com>
2022-06-18 12:56:19 +0900

    wasmのファイル名が変更されたので追加

---
+++ wasm/pkg/wasm.d.ts
@@ -0,0 +1,30 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function main_js(): void;
+/**
+*/
+export class CounterState {
+  free(): void;
+/**
+* @returns {CounterState}
+*/
+  static new(): CounterState;
+/**
+* @returns {number}
+*/
+  increment_counter(): number;
+/**
+* @returns {number}
+*/
+  decrement_counter(): number;
+/**
+* @returns {number}
+*/
+  get_counter(): number;
+/**
+* @param {number} new_counter
+*/
+  set_counter(new_counter: number): void;
+}
---
+++ wasm/pkg/wasm_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_counterstate_free(a: number): void;
+export function counterstate_new(): number;
+export function counterstate_increment_counter(a: number): number;
+export function counterstate_decrement_counter(a: number): number;
+export function counterstate_get_counter(a: number): number;
+export function counterstate_set_counter(a: number, b: number): void;
+export function main_js(): void;
+export function __wbindgen_start(): void;
